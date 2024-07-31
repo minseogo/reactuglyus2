47d34b10fb480435267529e269c34f4fe26738c4
minseogo <gominseo1112@gmail.com>
2024-07-31 16:57:54 +0900

    리뷰등장

--- src/component/pyj/quick/quickitem.js
+++ src/component/pyj/quick/quickitem.js
@@ -1,19 +1,19 @@
 function Quickitem() {
     const menuItems = [
-        { quickhref: '#', quickSrc: './img/img_pyj/quick/mypage.svg', quickAlt: '마이페이지', quicktext: '마이페이지' },
-        { quickhref: '#', quickSrc: './img/img_pyj/quick/market.svg', quickAlt: '싱싱마켓', quicktext: '싱싱마켓' },
-        { quickhref: '#', quickSrc: './img/img_pyj/quick/event.svg', quickAlt: '이벤트', quicktext: '이벤트' },
-        { quickhref: '#', quickSrc: './img/img_pyj/quick/service.svg', quickAlt: '고객센터', quicktext: '고객센터' }
+        { quickhref: '#', quickSrc: './img/img_pyj/quick/mypage.svg', quickAlt: '마이페이지', quickText: '마이페이지' },
+        { quickhref: '#', quickSrc: './img/img_pyj/quick/market.svg', quickAlt: '싱싱마켓', quickText: '싱싱마켓' },
+        { quickhref: '#', quickSrc: './img/img_pyj/quick/event.svg', quickAlt: '이벤트', quickText: '이벤트' },
+        { quickhref: '#', quickSrc: './img/img_pyj/quick/service.svg', quickAlt: '고객센터', quickText: '고객센터' }
     ];
     return (
         <>
             {
-                menuItems.map((v, idx)=>{
+                menuItems.map((v, i)=>{
                     return(
-                        <li>
+                        <li key={`quick${i}`}>
                             <a href={v.quickhref}>
                                 <img src={v.quickSrc} alt={v.quickAlt} />
-                                <span className='font_button_pyj'>{v.quicktext}</span>
+                                <span className='font_button_pyj'>{v.quickText}</span>
                             </a>
                         </li>
                     )
